74ffa57b9f92c2a4736d0a73fb6a442117f9ddfe
youssefshawky0 <youssefshawky691@gmail.com>
2024-03-05 16:18:29 +0200

    init

--- task_2/Question_2.dart
+++ task_2/Question_2.dart
@@ -3,9 +3,9 @@ void main() {
   var sum, subtract;
   var list = [10, 20, 30, 40];
   sum = (list.elementAt(0) + list.elementAt(1));
-  subtract = (list.elementAt(3) - list.elementAt(2));
+  subtract = (list.elementAt(2) - list.elementAt(3));
   print("The sum of the first and second is:$sum");
-  print("The sum of the first and second is:$subtract");
+  print("The subtract the fourth from the third is:$subtract");
 
   print("------------------END------------------------");
 }
