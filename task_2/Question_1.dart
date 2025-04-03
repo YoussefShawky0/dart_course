0c5bb62d80de027a07e8476e544604de49c03e32
Youssef Shawky <117600621+YoussefShawky0@users.noreply.github.com>
2025-04-03 12:59:17 +0200

    Update Question_1.dart

--- task_2/Question_1.dart
+++ task_2/Question_1.dart
@@ -1,10 +1,12 @@
 void main() {
+  
   print("-----------------Question_1----------------------");
 
-  var personName = 'youssef';
+  var personName = 'Youssef';
   var age = 19;
   print("My name is: $personName");
   print("My age is:  $age");
 
   print("------------------END------------------------");
+  
 }
